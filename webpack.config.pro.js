bd4f316687eae85f1238fa6ae44b44872472f4a8
IC-Tech <ic.imesh.chamara@gmail.com>
2020-07-08 05:22:16 +0530

    release preview

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -77,6 +77,7 @@ module.exports = merge(common, {
       'about',
       'stats',
       //'faq',
+      'one',
     ].map(a => new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: a + '.html',
