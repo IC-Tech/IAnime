d447c4b629423748735ef0f9e4f7c68d1d79e933
Imesh Chamara <ic.imesh.chamara@gmail.com>
2020-07-24 19:58:56 +0530

    remove old files

--- src/faq.js
+++
@@ -1,8 +0,0 @@
-/* Copyright © 2020, Imesh Chamara. All rights reserved. */
-import {gtag} from './comm.js'
-import com from './common_page.js'
-import './faq.scss'
-
-document.addEventListener('DOMContentLoaded', _ => com([
-	{t:'span', cl: 'title', txt: 'FAQ'},
-], {title: 'FAQ'}))
