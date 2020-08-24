0e0e9d4331fc3b71bdc1ab581205357633929c67
Imesh Chamara <ic.imesh.chamara@gmail.com>
2020-08-24 16:32:17 +0530

    create user page

---
+++ src/pages/user.js
@@ -0,0 +1,19 @@
+/* Copyright © 2020, Imesh Chamara. All rights reserved. */
+import {meta_init} from '../meta'
+import {page} from '../page'
+import '../style/user.scss'
+
+class user extends page {
+	constructor() {
+		super()
+		this.name = 'user'
+	}
+	load() {
+		meta_init(0, 'user')
+	}
+	content() {
+		return ([
+		])
+	}
+}
+export {user}
