23aa455cd2c4cded9bce3b3584078b01e6a6cf32
Imesh Chamara <ic.imesh.chamara@gmail.com>
2020-08-27 00:49:22 +0530

    Update user page

--- src/pages/user.js
+++ src/pages/user.js
@@ -19,47 +19,58 @@ class user extends page {
 			if(!a) return this.switchPage('nope')
 			this.load_ = 0
 			console.log(a)
+			a.title = a.display_name || a.name || (a.id && ('#' + a.id)) || ''
+			this.u = a
 			this.update()
+			meta_init(0, a.title + "'s " + (this.ops[this.ui].title || this.ops[this.ui].name), a.title + "'s IAnime user profile", a.poster, a.web)
 		}
 		this.ops = [
-			{id: 'overview', name: 'Overview', page: '', render: a => []},
+			{id: 'overview', title: 'Profile', name: 'Overview', page: '', render: a => [{t: 'div', cl: 'des-c', ch: [
+				{t: 'span', cl: 'des', txt: (a && a.description) || ''}
+			]}]},
 			{id: 'favorites', name: 'Favorites', page: 'favorites', render: a => []},
-			{id: 'bookmarks', name: 'Bookmarks', page: 'bookmarks', render: a => []},
+			{id: 'bookmarks', name: 'Bookmarks', page: 'bookmarks', p: 1, render: a => []},
 			{id: 'followings', name: 'Followings', page: 'followings', render: a => []},
 			{id: 'followers', name: 'Followers', page: 'followers', render: a => []},
 		]
 	}
 	load(a) {
-		this.u = {}
+		this.u = null
 		this.ui = 0
 		this._load = 1
 		this.load_ = 1
 		this.init = 0
-		this.parse(a.ex)
+		a.ui = a.dirs[2] || ''
+		if(!this.ops.some((b,c) => b.page == a.ui ? [this.ui = c] : 0)) return this.switchPage('nope')
+		this.parse(this.id = a.ex)
 		console.log(a)
 		this.update()
 	}
 	content() {
-		var a = this.user || {}
+		var a = this.u || {}
+		var b = a => [this.load_ ? 'skeleton' : 'K', a]
+		var c = a => this.load_ ? '' : a
+		var c_ = a => this.load_ ? undefined : a
 		return ([
 			{t: 'div', cl: 'cont', ch: [
-				{t: 'div', at: {role: 'img', 'aria-label': 'Profile banner'}, cl: 'bann-c', s: {'background-image': `url(${(a.user && a.user.banner) || '/images/default/banner_3_unoptimised.jpg'})`}},
+				{t: 'div', at: {role: 'img', 'aria-label': 'Profile banner'}, cl: b('bann-c'), s: {'background-image': c(`url(${(a && a.banner) || '/images/default/banner_3_unoptimised.jpg'})`)}},
 				{t: 'div', cl: 'scon', ch: [
 					{t: 'div', cl: 'si1', ch: [
-						{t: 'div', cl: 'pos', s: {'background-image': `url(${(a.user && a.user.poster) || '/images/default/avatar_op.jpg'})`}},
+						{t: 'div', cl: b('pos'), at: {role: 'img', title: c(a && a.title && (a.title + "'s Avatar"))}, s: {'background-image': c(`url(${(a && a.poster) || '/images/default/avatar_op.jpg'})`)}},
 					]},
 					{t: 'div', cl: 'si2', ch: [
-						{t: 'span', cl: 'name', txt: (a.user && a.user.display_name) || 'Imesh'},
+						{t: 'span', cl: b('name'), txt: c(a && a.title)},
 					]}
 				]},
 				{t: 'div', cl: 'tab-c', ch: [
 					{t: 'div', cl: 'hld'},
 					{t: 'div', cl: 'tab-c2', ch: [
-						{t: 'div', cl: 'tabs', ch: this.ops.map((c,b) => ({t: 'a', cl: ['tab', this.ui == b ? 'sel' : 'K'], at: {href: '/user/' + ((a.user && a.user.id) || 1) + (c.page ? '/' + c.page : '')}, txt: c.name}))}
+						{t: 'div', cl: b('tabs'), ch: this.ops.map((c,b) => ({t: 'a', s: {display: (c.p && !(a && a.self)) ? 'none' : 'block'}, cl: ['tab', this.ui == b ? 'sel' : 'K'], at: {href: c_('/user/' + ((a && a.name || a.id) || 0) + (c.page ? '/' + c.page : ''))}, txt: c.name}))}
 					]}
 				]},
 			]},
-			{t: 'div', cl: ['cont-r', this.ops[this.ui].id], ch: this.ops[this.ui].render(this)}
+			{t: 'div', cl: ['cont-r', this.ops[this.ui].id], ch: this.load_ ? [] : this.ops[this.ui].render(a, this)},
+			{t: 'div', cl: nope(this.ui == 0, 'feed'), at: {role: 'feed', 'aria-busy': !!this.load_}, ch: this.load_ ? [] : [{t: 'span', cl: 'temp', txt: `user haven't create any ${a && a.self ? '' : 'public '}activities to display`}]}
 		])
 	}
 }
